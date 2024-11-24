7a718fd8f363f8ff334bcc62c7b0fa9176e09350
hasan02 <hasanarifyigit@gmail.com>
2024-11-24 17:06:49 +0300

    git menü oldu

--- Assets/Mainmenu.cs
+++ Assets/Mainmenu.cs
@@ -1,64 +1,12 @@
-// Created by NightTime Developments!
-
 using System.Collections;
-
 using System.Collections.Generic;
-
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-using UnityEngine.UI;
-
-
-public class Mainmenu : MonoBehaviour
-
+public class MainMenu : MonoBehaviour
 {
-
-    public GameObject mainmenue;
-
-    public GameObject Player;
-
-    public GameObject MenuCam;
-
-    // Start is called before the first frame update
-
-    public void Play()
-
+    public void PlayGame()
     {
-
-        Player.SetActive(false);
-
-        mainmenue.SetActive(true);
-
+        SceneManager.LoadSceneAsync(1);
     }
-
-
-    // Update is called once per frame
-
-    /* void Update()
-
-     {
-
-
-
-     }*/
-
-
-    public void PlayButton()
-
-    {
-
-        mainmenue.SetActive(false);
-
-        Cursor.lockState = CursorLockMode.Locked;
-
-        Cursor.visible = false;
-
-        Player.SetActive(true);
-
-        MenuCam.SetActive(false);
-
-    }
-
 }
-
-
