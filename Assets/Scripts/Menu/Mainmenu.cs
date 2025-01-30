92a1047a9c1aed1cec24787845b8c66804a9ea99
mushuacan <mushuacan@gmail.com>
2025-01-30 22:15:15 +0300

    Menu Credits Arranged
    Ulan şunu yapmaktan ne zevk aldım haaaopwkaopdkpwaokdpawkdawokdawkdpwoako

--- Assets/Scripts/Menu/Mainmenu.cs
+++ Assets/Scripts/Menu/Mainmenu.cs
@@ -13,5 +13,8 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
